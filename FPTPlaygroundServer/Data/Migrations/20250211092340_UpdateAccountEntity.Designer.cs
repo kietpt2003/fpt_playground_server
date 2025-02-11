7c2771f337d2288c93c52eb0f3d5da5d0699ae1f
kietpt2003 <tuankiet29012003@gmail.com>
2025-02-11 16:27:00 +0700

    Update Account Entity

--- FPTPlaygroundServer/Data/Migrations/20250211092340_UpdateAccountEntity.Designer.cs
+++ FPTPlaygroundServer/Data/Migrations/20250211092340_UpdateAccountEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FPTPlaygroundServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FPTPlaygroundServer.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250211092340_UpdateAccountEntity")]
+    partial class UpdateAccountEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FPTPlaygroundServer/Data/Migrations/20250211092340_UpdateAccountEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPTPlaygroundServer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAccountEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Accounts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Accounts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Accounts");
+        }
+    }
+}
