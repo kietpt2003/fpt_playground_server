de2f65faf001aa9669ea8fbfd2426c1baaa76065
kietpt2003 <tuankiet29012003@gmail.com>
2025-02-27 20:31:16 +0700

    Update send message

--- FPTPlaygroundServer/Services/Background/Chats/SendMessageService .cs
+++ FPTPlaygroundServer/Services/Background/Chats/SendMessageService .cs
@@ -11,31 +11,57 @@ public class SendMessageService(IConnectionMultiplexer redis, IHubContext<ChatHu
 {
     private readonly IHubContext<ChatHub> _hub = hub;
     private readonly IConnectionMultiplexer _redis = redis;
+    private bool _isSubscribed = false;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        if (_isSubscribed)
         {
-            try
-            {
-                var subscriber = _redis.GetSubscriber();
-                await subscriber.SubscribeAsync(RedisChannels.ChatChannel.ToString(), async (channel, message) =>
-                {
-                    // Khi nhận tin nhắn từ Redis, deserialize và phát tin nhắn qua SignalR
-                    var chatMessage = JsonSerializer.Deserialize<Message>(message);
-                    if (chatMessage != null)
-                    {
-                        await _hub.Clients.Group(chatMessage.ConversationId.ToString())
-                            .SendAsync("GroupMethod", chatMessage, stoppingToken);
-                    }
-                });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error processing message: {ex.Message}");
-            }
-
-            await Task.Delay(1000, stoppingToken); // Chờ 1s trước khi tiếp tục xử lý
+            return;
         }
+        var subscriber = _redis.GetSubscriber();
+
+        try
+        {
+            await subscriber.SubscribeAsync(RedisChannels.ChatChannel.ToString(), async (channel, message) =>
+            {
+                // Khi nhận tin nhắn từ Redis, deserialize và phát tin nhắn qua SignalR
+                var chatMessage = JsonSerializer.Deserialize<Message>(message);
+                if (chatMessage != null)
+                {
+                    await _hub.Clients.Group(chatMessage.ConversationId.ToString())
+                        .SendAsync("GroupMethod", chatMessage, stoppingToken);
+                }
+            });
+
+            _isSubscribed = true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error processing message: {ex.Message}");
+        }
+        //while (!stoppingToken.IsCancellationRequested)
+        //{
+        //    try
+        //    {
+        //        var subscriber = _redis.GetSubscriber();
+        //        await subscriber.SubscribeAsync(RedisChannels.ChatChannel.ToString(), async (channel, message) =>
+        //        {
+        //            // Khi nhận tin nhắn từ Redis, deserialize và phát tin nhắn qua SignalR
+        //            var chatMessage = JsonSerializer.Deserialize<Message>(message);
+        //            if (chatMessage != null)
+        //            {
+        //                await _hub.Clients.Group(chatMessage.ConversationId.ToString())
+        //                    .SendAsync("GroupMethod", chatMessage, stoppingToken);
+        //            }
+        //        });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"Error processing message: {ex.Message}");
+        //    }
+
+        //    await Task.Delay(1000, stoppingToken); // Chờ 1s trước khi tiếp tục xử lý
+        //}
     }
 }
